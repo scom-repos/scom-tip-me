54536522ef4b21eec53447f9ebe16f726900044f
Bryan <bryan.dev@instajs.com>
2023-08-03 16:34:53 +0700

    Convert state into a class

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,88 +2,59 @@ import { INetwork, Wallet } from "@ijstech/eth-wallet";
 import { application } from "@ijstech/components";
 
 export const enum EventId {
-  chainChanged = 'chainChanged',
   Paid = 'Paid'
 }
 
-export enum WalletPlugin {
-  MetaMask = 'metamask',
-  WalletConnect = 'walletconnect',
-}
+export class State {
+  ipfsGatewayUrl: string = '';
+  rpcWalletId: string = '';
 
-export const getNetworkInfo = (chainId: number) => {
-  return state.networkMap[chainId];
-}
-
-export const getSupportedNetworks = () => {
-  return Object.values(state.networkMap);
-}
-
-export const state = {
-  networkMap: {} as { [key: number]: INetwork },
-  ipfsGatewayUrl: 'https://ipfs.scom.dev/ipfs/',
-  rpcWalletId: ''
-}
-
-export const setDataFromSCConfig = (options: any) => {
-  if (options.ipfsGatewayUrl) {
-    setIPFSGatewayUrl(options.ipfsGatewayUrl);
+  constructor(options: any) {
+    this.initData(options);
   }
-}
 
-export const setIPFSGatewayUrl = (url: string) => {
-  state.ipfsGatewayUrl = url;
-}
+  private initData(options: any) {
+    if (options.ipfsGatewayUrl) {
+      this.ipfsGatewayUrl = options.ipfsGatewayUrl;
+    }
+  }
 
-export const getIPFSGatewayUrl = () => {
-  return state.ipfsGatewayUrl;
-}
+  initRpcWallet(defaultChainId: number) {
+    if (this.rpcWalletId) {
+      return this.rpcWalletId;
+    }
+    const clientWallet = Wallet.getClientInstance();
+    const networkList: INetwork[] = Object.values(application.store?.networkMap || []);
+    const instanceId = clientWallet.initRpcWallet({
+      networks: networkList,
+      defaultChainId,
+      infuraId: application.store?.infuraId,
+      multicalls: application.store?.multicalls
+    });
+    this.rpcWalletId = instanceId;
+    if (clientWallet.address) {
+      const rpcWallet = Wallet.getRpcWalletInstance(instanceId);
+      rpcWallet.address = clientWallet.address;
+    }
+    return instanceId;
+  }
 
-export const getImageIpfsUrl = (url: string) => {
-  if (url && url.startsWith("ipfs://"))
-    return `${getIPFSGatewayUrl()}${url.substring(7)}`;
-  return url;
+  getRpcWallet() {
+    return this.rpcWalletId ? Wallet.getRpcWalletInstance(this.rpcWalletId) : null;
+  }
+
+  isRpcWalletConnected() {
+    const wallet = this.getRpcWallet();
+    return wallet?.isConnected;
+  }
+
+  getChainId() {
+    const rpcWallet = this.getRpcWallet();
+    return rpcWallet?.chainId;
+  }
 }
 
 export function isClientWalletConnected() {
   const wallet = Wallet.getClientInstance();
   return wallet.isConnected;
-}
-
-export function isRpcWalletConnected() {
-  const wallet = getRpcWallet();
-  return wallet?.isConnected;
-}
-
-export function getChainId() {
-  const rpcWallet = getRpcWallet();
-  return rpcWallet?.chainId;
-}
-
-export function initRpcWallet(defaultChainId: number) {
-  if (state.rpcWalletId) {
-    return state.rpcWalletId;
-  }
-  const clientWallet = Wallet.getClientInstance();
-  const networkList: INetwork[] = Object.values(application.store.networkMap);
-  const instanceId = clientWallet.initRpcWallet({
-    networks: networkList,
-    defaultChainId,
-    infuraId: application.store.infuraId,
-    multicalls: application.store.multicalls
-  });
-  state.rpcWalletId = instanceId;
-  if (clientWallet.address) {
-    const rpcWallet = Wallet.getRpcWalletInstance(instanceId);
-    rpcWallet.address = clientWallet.address;
-  }
-  return instanceId;
-}
-
-export function getRpcWallet() {
-  return Wallet.getRpcWalletInstance(state.rpcWalletId);
-}
-
-export function getClientWallet() {
-  return Wallet.getClientInstance();
 }
